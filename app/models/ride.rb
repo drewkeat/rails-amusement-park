daa5ddff03a1b01d1d63e6a6d26f9454966bf6f2
Drew Keat <thedrewkeat@gmail.com>
2021-08-26 14:28:27 -0600

    add model relationships and seed data

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,10 @@
 class Ride < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :attraction
+
+    def take_ride
+        user = User.find(self.user_id)
+        attraction = Attraction.find(self.attraction_id)
+        
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
     validates :password, presence: true
-    has_many :rides 
+    has_many :rides
+    has_many :attractions, through: :rides
+
+    def mood
+        if self.nausea > self.happiness
+            return 'sad'
+        else
+            return 'happy'
+        end
+    end
 end
