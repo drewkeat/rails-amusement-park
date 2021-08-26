7bb8b56f0fc3e21f5aa4027e687fdfd583f81a55
Drew Keat <thedrewkeat@gmail.com>
2021-08-26 14:16:51 -0600

    generate CreateAttractions migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
     validates :password, presence: true
+    has_many :rides 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_26_200753) do
+ActiveRecord::Schema.define(version: 2021_08_26_201624) do
+
+  create_table "attractions", force: :cascade do |t|
+    t.string "name"
+    t.integer "tickets"
+    t.integer "nausea_rating"
+    t.integer "happiness_rating"
+    t.integer "min_height"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
