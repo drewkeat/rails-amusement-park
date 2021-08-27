c52f97659df12835bb5f57f4e50fc490e07e99d6
Drew Keat <thedrewkeat@gmail.com>
2021-08-27 15:58:03 -0600

    create users layout, add logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,5 +18,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session.clear
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 Rails.application.routes.draw do
   root'sessions#home'
-  get 'attractions/index'
-  get 'attractions/new'
-  get 'attractions/create'
-  get 'attractions/show'
-  get 'attractions/edit'
-  get 'attractions/update'
-  get 'attractions/destroy'
+
   get '/signin', to: 'sessions#new'
   post '/session', to: 'sessions#create'
-  get 'sessions/destroy'
+  get '/logout', to: 'sessions#destroy', as: :logout
   
   resources :users
-
+  resources :attractions
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
