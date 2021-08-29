42c92a89655cb8b8097ef3cb7b175f60356f0693
Drew Keat <thedrewkeat@gmail.com>
2021-08-28 21:38:45 -0600

    create attractions form partial

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -4,9 +4,18 @@ class AttractionsController < ApplicationController
   end
 
   def new
+    @attraction = Attraction.new
   end
 
   def create
+    @attraction = Attraction.new(attraction_params)
+    if @attraction.save
+      flash[:success] = "Attraction successfully created"
+      redirect_to @attraction
+    else
+      flash[:error] = "Something went wrong"
+      render 'new'
+    end
   end
 
   def show
@@ -14,11 +23,22 @@ class AttractionsController < ApplicationController
   end
 
   def edit
+    @attraction = Attraction.find(params[:id])
   end
 
   def update
+    @attraction = Attraction.find(params[:id])
+    @attraction.update(attraction_params)
+    @attraction.save
+    redirect_to @attraction
   end
 
   def destroy
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
   end
 end
